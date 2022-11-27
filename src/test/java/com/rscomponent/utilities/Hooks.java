63ef8ccecd6f486f3ec3bb68bf50d072c3cd1c2b
Karthik Veeranan <karthikveer.qa@gmail.com>
2022-11-27 16:01:33 +0000

    updated cucumber report

--- src/test/java/com/rscomponent/utilities/Hooks.java
+++ src/test/java/com/rscomponent/utilities/Hooks.java
@@ -3,11 +3,15 @@ package com.rscomponent.utilities;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 
 import java.util.logging.Logger;
 
 public class Hooks {
-
+    private WebDriver webDriver;
+    BasePage basePage;
     private static final Logger logger = Logger.getLogger(Hooks.class.getName());
 
     @Before
@@ -17,10 +21,10 @@ public class Hooks {
 
     @After
     public void afterScenario(Scenario scenario) {
-//        if (scenario.isFailed()) {
-//            byte[] screenshot = ((TakesScreenshot) ).getScreenshotAs(OutputType.BYTES);
-//            scenario.embed(screenshot, "image/png");
-//        }
-//        logger.info("###########" + scenario.getName() + " Finished with status  " + scenario.getStatus() + "##########");
+       if (scenario.isFailed()) {
+           byte[] screenshot = ((TakesScreenshot) basePage.getWebDriver() ).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
+        }
+        logger.info("###########" + scenario.getName() + " Finished with status  " + scenario.getStatus() + "##########");
     }
 }
